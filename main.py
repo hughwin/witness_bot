c12b013ac68c17aaca9b536e5bad9da319146099
hughwin <hugh.winchester@gmail.com>
2021-09-17 16:23:44 +0100

    Reddit basic log in established

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import os
+import praw
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 def start():
-    return 1
+    reddit = praw.Reddit(
+        client_id=os.getenv("CLIENT_ID"),
+        client_secret=os.getenv("CLIENT_SECRET"),
+        password=os.getenv("PASSWORD"),
+        user_agent=os.getenv("USER_AGENT"),
+        username=os.getenv("REDDIT_USERNAME"),
+    )
+    print(reddit.user.me())
 
 
 if __name__ == '__main__':
